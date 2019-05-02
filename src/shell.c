ba661a2ee5d69a8086a3981b9f5983974d4fc703
Gabriel de Oliveira Neri <neri.gabriel78@gmail.com>
2019-05-02 14:11:35 -0300

    feat: add login

--- src/shell.c
+++ src/shell.c
@@ -1,1 +1,23 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/wait.h>
 
+int main (int argc, char * argv[]){
+  char user [30];
+  char pass [30];
+
+  printf("Entre com o usuario: ");
+  gets(user);
+  printf("Entre com a senha: ");
+  gets(pass);
+
+   if(strcmp(user, "root") != 0 || strcmp(pass, "fatecso") != 0 ){
+     printf("Usuario ou senha invalidos \n");
+     exit(0);
+   }
+}
