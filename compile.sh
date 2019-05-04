5a675d2c499cc49875729fd0afc13dbe8df5e083
neriGabriel <neri.gabriel78@gmail.com>
2019-05-04 18:44:18 -0300

    feat: add shell.c, compile.sh and .gitignore

--- compile.sh
+++ compile.sh
@@ -1,1 +1,4 @@
 #!/usr/bin/env bash
+echo clear
+gcc ./src/ajuda.c -o ajuda
+gcc ./src/shell.c -o shell
--- src/shell.c
+++ src/shell.c
@@ -8,16 +8,32 @@
 #include <sys/wait.h>
 
 int main (int argc, char * argv[]){
-  char user [30];
-  char pass [30];
+  char user [30], pass [30], comando[30], *arg[3];
+  int pid;
 
   printf("Entre com o usuario: ");
   gets(user);
   printf("Entre com a senha: ");
   gets(pass);
 
-   if(strcmp(user, "root") != 0 || strcmp(pass, "fatecso") != 0 ){
+   if(strcmp(user, "root") != 0 || strcmp(pass, "fatecso") != 0 )
+   {
      printf("Usuario ou senha invalidos \n");
      exit(0);
+   }
+
+   for(;;)
+   {
+      printf("fatec> ");
+      gets(comando);
+      argv[0] = strtok(comando, "");
+      argv[1] = strtok(NULL, "");
+      argv[2] = NULL;
+
+      //módulo de sair
+      if(strcmp(argv[0], "sair") == 0) exit(0);
+
+      //módulo de ajuda
+      if(strcmp(argv[0], "ajuda") == 0){}
    }
 }
