d4fb28d02155590365487fe60e5ee61535865630
Gabriel de Oliveira Neri <neri.gabriel78@gmail.com>
2019-05-06 11:20:10 -0300

    feat: add example to shellscript

--- compile.sh
+++ compile.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
-echo clear
-gcc ./src/ajuda.c -o ajuda
-gcc ./src/shell.c -o shell
+echo "COMPILANDO ARQUIVOS...."
+
+gcc ajuda.c -o ajuda
+if [ $? -eq 1 ]
+then
+    exit "Erro ao compilar o arquivo de ajuda!"
+fi
+
+
+gcc main.c -o main
+if [ $? -eq 1 ]
+then
+    echo "Erro ao compilar o arquivo principal!"
+fi
+
+echo "ARQUIVOS COMPILADOS COM SUCESSO!"
