afacdeeb59b2104787d0c4331928a6296bd10c78
Kaio Rauta <kaio_soares_rauta@yahoo.com.br>
2019-05-07 11:25:53 -0300

    Algumas mudanças se sobreescreveram, só coloquei as minhas depois das feitas

--- compile.sh
+++ compile.sh
@@ -4,27 +4,107 @@ echo "COMPILANDO ARQUIVOS...."
 chmod -R 777 ./src
 chmod -R 777 ./app
 
+#= Compilando o arquivo de ajuda
 gcc ./src/ajuda.c -o ./app/ajuda
-$? -> pega o resultado do ultimo comando executado [ 0 = sucesso , 1 = erro]
 if [ $? -eq 1 ]
 then
     exit "Erro ao compilar o arquivo de ajuda!"
 fi
+##################################
 
+#= Compilando o arquivo de apagar
 gcc ./src/apagar.c -o ./app/apagar
 $? -> pega o resultado do ultimo comando executado [ 0 = sucesso , 1 = erro]
 if [ $? -eq 1 ]
 then
     exit "Erro ao compilar o arquivo de ajuda!"
 fi
+##################################
+
+#= Compilando o arquivo de copiar
+gcc ./src/copiar.c -o ./app/copiar
+if [ $? -eq 1 ]
+then
+    exit "Erro ao compilar o arquivo de copiar!"
+fi
+##################################
+
+#= Compilando o arquivo de files
+gcc ./src/arquivo.c -o ./app/arquivo
+if [ $? -eq 1 ]
+then
+    exit "Erro ao compilar o arquivo de files!"
+fi
+##################################
+
+#= Compilando o arquivo de créditos
+gcc ./src/dev.c -o ./app/dev
+if [ $? -eq 1 ]
+then
+    exit "Erro ao compilar o arquivo de créditos!"
+fi
+##################################
+
+#= Compilando o arquivo de listar
+gcc ./src/listar.c -o ./app/listar
+if [ $? -eq 1 ]
+then
+    exit "Erro ao compilar o arquivo de listar!"
+fi
+##################################
+
+#= Compilando o arquivo de criar
+gcc ./src/criar.c -o ./app/criar
+if [ $? -eq 1 ]
+then
+    exit "Erro ao compilar o arquivo de criar!"
+fi
+###################################
 
 
-gcc ./src/shell.c -o ./app/shell
+#= Compilando o arquivo de data
+gcc ./src/data.c -o ./app/data
+if [ $? -eq 1 ]
+then
+    exit "Erro ao compilar o arquivo de data!"
+fi
+################################
+
+#= Compilando o arquivo de local
+gcc ./src/local.c -o ./app/local
+if [ $? -eq 1 ]
+then
+    exit "Erro ao compilar o arquivo de local!"
+fi
+#################################
+
+#= Compilando o arquivo de mudar
+gcc ./src/mudar.c -o ./app/mudar
+if [ $? -eq 1 ]
+then
+    exit "Erro ao compilar o arquivo de mudar!"
+fi
+##################################
+
+#= Compilando o arquivo de remover
+gcc ./src/remover.c -o ./app/remover
+if [ $? -eq 1 ]
+then
+    exit "Erro ao compilar o arquivo de remover!"
+fi
+###################################
+
+#= Compilando o arquivo principal
+gcc ./src/shell.c -o shell
 if [ $? -eq 1 ]
 then
     exit "Erro ao compilar o arquivo principal!"
 fi
-
+#####################################
 
 chmod -R 777 ./app
 echo "ARQUIVOS COMPILADOS COM SUCESSO!"
+
+echo "EXECUTANDO ARQUIVO...."
+clear
+./shell
