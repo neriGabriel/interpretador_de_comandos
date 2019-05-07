d0722681c3b96f9b7fd5941df618920bdb3bcb71
Kaio Rauta <kaio_soares_rauta@yahoo.com.br>
2019-05-07 13:37:33 -0300

    Update remover.c
    Código para remover pastas está pronto.

--- src/remover.c
+++ src/remover.c
@@ -1,5 +1,73 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <locale.h>
 
-int main () {
+#define PATH_SIZE 260
 
+void limparTela()
+{
+    system("cls||clear");
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int cliffHanger = 1, error, tamanhoNome;
+    char local[PATH_SIZE];
+
+    getcwd(local, PATH_SIZE);
+
+    if (local != NULL)
+    {
+        printf("O diretório atual é: %s\n", local);
+    }
+    else
+    {
+        perror("Erro: ");
+        printf("\n");
+        return 1;
+    }
+
+    do
+    {
+        error = 0;
+        tamanhoNome = 0;
+        printf("\nDigite exit a qualquer momento para sair...\n");
+        printf("\nQual pasta gostaria de apagar: ");
+        fflush(stdin);
+        fgets(local, PATH_SIZE, stdin);
+
+        tamanhoNome = strlen(local) - 1;
+
+        if (local[tamanhoNome] == '\n')
+        {
+            local[tamanhoNome] = '\0';
+        }
+
+        if (strcmp(local, "cls") == 0 || strcmp(local, "clear") == 0)
+        {
+            limparTela();
+            main();
+        }
+
+        if (strcmp(local, "exit") == 0)
+        {
+            exit(1);
+        }
+
+        if ((error = rmdir(local)) == 0)
+        {
+            printf("Diretório excluido com sucesso!\n");
+        }
+        else
+        {
+            perror("Erro: ");
+            printf("\n");
+        }
+
+    } while (cliffHanger == 1);
+
+    return 0;
 }
