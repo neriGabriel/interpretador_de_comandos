b06016ac7b2a1697d99c659194d8f52d55516f93
neriGabriel <neri.gabriel78@gmail.com>
2019-05-07 19:29:16 -0300

    commit emergencial

--- src/ajuda.c
+++ src/ajuda.c
@@ -2,4 +2,16 @@
 
 int main(){
   printf("\t\t **** AJUDA SHELL FATEC ****\n");
+  printf("\t\t **** mudar   [ cd    ] ****\n");
+  printf("\t\t **** local   [ pwd   ] ****\n");
+  printf("\t\t **** apagar  [ rm    ] ****\n");
+  printf("\t\t **** criar   [ mkdir ] ****\n");
+  printf("\t\t **** remover [ rmdir ] ****\n");
+  printf("\t\t **** copiar  [ cp    ] ****\n");
+  printf("\t\t **** data    [ date  ] ****\n");
+  printf("\t\t **** listar  [ ls    ] ****\n");
+  printf("\t\t **** arquivo [ touch ] ****\n");
+  printf("\t\t **** dev     [ cŕeditos ] ****\n");
+  printf("\t\t **** sair    [ sair  ] ****\n");
+  return 0;
 }
