08e0542fcef918804c52ff7a6f8ce9bbeebee48e
Gabriel de Oliveira Neri <neri.gabriel78@gmail.com>
2019-05-08 17:00:33 -0300

    Update ajuda.c

--- src/ajuda.c
+++ src/ajuda.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
-int main(){
-  printf("\t\t **** AJUDA SHELL FATEC ****\n");
+int main() {
+  printf("\t\t ==== AJUDA SHELL FATEC ==== \n");
+  printf("\t\t ==== apagar  = rm      ==== \n");
+  printf("\t\t ==== arquivo = touch   ==== \n");
+  printf("\t\t ==== copiar  = cp      ==== \n");
+  printf("\t\t ==== criar   = mkdir   ==== \n");
+  printf("\t\t ==== data    = date    ==== \n");
+  printf("\t\t ==== dev     = devs    ==== \n");
+  printf("\t\t ==== listar  = ls      ==== \n");
+  printf("\t\t ==== local   = pwd     ==== \n");
+  printf("\t\t ==== mudar   = cd      ==== \n");
+  printf("\t\t ==== remover = rmdir   ==== \n");
+  printf("\t\t =========================== \n");
+  return 0;
 }
