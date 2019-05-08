076d8739d04e8064a99ff2f4d8bac56795937326
Gabriel de Oliveira Neri <neri.gabriel78@gmail.com>
2019-05-08 16:56:23 -0300

    Update local.c

--- src/local.c
+++ src/local.c
@@ -5,7 +5,6 @@
 int main()
 {
    char cwd[PATH_MAX];
-   if (getcwd(cwd, sizeof(cwd)) != NULL)printf("Current working dir: %s\n", cwd);
+   if (getcwd(cwd, sizeof(cwd)) != NULL) printf("%s\n", cwd);
    return 0;
 }
-
