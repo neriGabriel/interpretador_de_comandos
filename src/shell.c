580acb9857a0a2ba2f9a6967e2a2225ac16db2f7
MisterKaiou <kaio_soares_rauta@yahoo.com.br>
2019-05-09 17:33:31 -0300

    Adicionado 3 tentativas ao logone e codigo formatado

--- src/shell.c
+++ src/shell.c
@@ -14,186 +14,217 @@
 
 char *token, *array[512];
 
-void makeTokens(char *input){
-   int i = 0;
-		token = strtok(input, "\n ");
-			while (token != NULL) {
-				array[i++] = token;
-				token = strtok(NULL, "\n ");
-			}
-		array[i] = NULL;
-}
-
-void getCredentials(char user[USER_SIZE], char pass[PASS_SIZE])
+void limpaTela()
 {
-  int SIZE = 0;
-
-  printf("Entre com o usuário: ");
-  fgets(user, USER_SIZE, stdin);
-  fflush(stdin);
-
-  SIZE = strlen(user) - 1;
-
-  if (user[SIZE] == '\n')
-      user[SIZE] = '\0';
-
-  printf("Entre com a senha: ");
-  fgets(pass, PASS_SIZE, stdin);
-  fflush(stdin);
-
-  SIZE = strlen(pass);
-
-  if (pass[SIZE] == '\n')
-      pass[SIZE] = '\0';
-
+    printf("\e[H\e[2J");
 }
 
-int main (int argc, char * argv[]){
-  char user [USER_SIZE], pass [PASS_SIZE], comando[COMANDO_SIZE];
-  int pid, i = 0;
+void makeTokens(char *input)
+{
+    int i = 0;
+    token = strtok(input, "\n ");
+    while (token != NULL)
+    {
+        array[i++] = token;
+        token = strtok(NULL, "\n ");
+    }
+    array[i] = NULL;
+}
 
-  setlocale (LC_ALL, "Portuguese");
+char getCredentials(char user[USER_SIZE], char pass[PASS_SIZE])
+{
+    int SIZE = 0, i = 0, correctName, correctPass;
 
-  printf("\e[H\e[2J");
+    for(i = 0; i <= 2; i++)
+    {
 
-  for(i = 0; i <= 2; i++)
-  {
+        correctName = 0; correctPass = 0;
+
+        printf("Entre com o usuário: ");
+        fgets(user, USER_SIZE, stdin);
+        fflush(stdin);
+
+        SIZE = strlen(user) - 1;
+
+        if (user[SIZE] == '\n')
+        {
+            user[SIZE] = '\0';
+        }
+
+        correctName = strcmp(user, "root");
+
+        printf ("O nome está %i\n", correctName);
+
+        printf("Entre com a senha: ");
+        fgets(pass, PASS_SIZE, stdin);
+        fflush(stdin);
+
+        SIZE = strlen(pass) - 1;
+
+        if (pass[SIZE] == '\n')
+        {
+            pass[SIZE] = '\0';
+        }
+
+        correctPass = strcmp(pass, "fatecso");
+
+        printf ("A senha está %i\n", correctPass);
+
+        printf("%s\n%s", user, pass);
+
+        if(i == 2)
+        {
+            printf("Excedeu a quantidade de tentativas\n\n");
+            exit(0);
+        }
+        if (correctName != 0 || correctPass != 0)
+        {
+            printf("Usuário ou senha inválidos\n\n");
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return user, pass;
+}
+
+int main (int argc, char * argv[])
+{
+
+    char user [USER_SIZE], pass [PASS_SIZE], comando[COMANDO_SIZE];
+    int pid, i = 0;
+
+    setlocale (LC_ALL, "Portuguese");
+
+    limpaTela();
 
     getCredentials(user, pass);
 
-    printf("%s\n%s", user, pass);
+    limpaTela();
 
-    if(i == 2)
+    printf("Seja bem vindo %s!\n", user);
+
+    for(;;)
     {
-      printf("Excedeu a quantidade de tentativas\n\n");
-      exit(0);
+        printf("fatec> ");
+        fgets(comando, COMANDO_SIZE, stdin);
+
+        makeTokens(comando);
+
+        argv[0] = strtok(comando, "");
+
+        if(strcmp(argv[0], "sair") == 0)
+        {
+            printf("Até mais %s =)\n", user);
+            exit(0);
+        }
+
+        else if(strcmp(argv[0], "limpar") == 0 || strcmp(argv[0], "clear") == 0) printf("\e[H\e[2J");
+
+        else if(strcmp(argv[0], "ajuda") == 0)
+        {
+            pid=fork();
+            if(pid==0) execvp("./app/ajuda", array);
+            else wait(NULL);
+            continue;
+        }
+
+        else if(strcmp(argv[0], "apagar") == 0)
+        {
+            pid=fork();
+            if(pid==0) execlp("./app/apagar", "./app/apagar", array, NULL);
+            else wait(NULL);
+            continue;
+        }
+
+        else if(strcmp(argv[0], "arquivo") == 0)
+        {
+            pid=fork();
+            if(pid==0) execvp("./app/arquivo", array);
+            else wait(NULL);
+            continue;
+        }
+
+        else if (strcmp(argv[0], "copiar") == 0)
+        {
+            pid = fork();
+            if(pid==0) execvp("./app/copiar", array);
+            else wait(NULL);
+            continue;
+        }
+
+        else if(strcmp(argv[0], "criar") == 0)
+        {
+            pid=fork();
+            if(pid==0) execvp("./app/criar", array);
+            else wait(NULL);
+            continue;
+        }
+
+        else if(strcmp(argv[0], "data") == 0)
+        {
+            pid=fork();
+            if(pid==0) execvp("./app/data", array);
+            else wait(NULL);
+            continue;
+        }
+
+        else if(strcmp(argv[0], "dev") == 0)
+        {
+            pid=fork();
+            if(pid==0) execlp("./app/dev", "./app/dev", array, NULL);
+            else wait(NULL);
+            continue;
+        }
+
+        else if (strcmp(argv[0], "listar") == 0)
+        {
+            pid = fork();
+            if(pid==0) execvp("./app/listar", array);
+            else wait(NULL);
+            continue;
+        }
+
+        else if (strcmp(argv[0], "local") == 0)
+        {
+            pid = fork();
+            if(pid == 0) execlp("./app/local", "./app/local", array, NULL);
+            else wait(NULL);
+            continue;
+        }
+
+        else if (strcmp(argv[0], "mudar") == 0)
+        {
+            pid = fork();
+            if(pid==0) execvp("./app/mudar", array);
+            else wait(NULL);
+            continue;
+        }
+
+        else if (strcmp(argv[0], "usuario") == 0)
+        {
+            pid = fork();
+            if(pid==0) execvp("./app/quem", array);
+            else wait(NULL);
+            continue;
+        }
+
+        else if(strcmp(argv[0], "remover") == 0)
+        {
+            pid=fork();
+            if(pid==0) execlp("./app/remover", "./app/remover", array, NULL);
+            else wait(NULL);
+            continue;
+        }
+
+        else if(comando[0] == '.' && comando[1] == '/')
+        {
+            system(comando);
+            continue;
+        }
+
+        else printf("comando: %s nao encontrado\n", comando);
     }
-    else if (strcmp(user, "root") != 0 || strcmp(pass, "fatecso") != 0 )
-    {
-      printf("Usuário ou senha inválidos\n\n");
-    }
-    else
-    {
-      continue;
-    }
-  }
-void limpaTela(){
-  printf("\e[H\e[2J");
-}
-
-int main (int argc, char * argv[]){
-  char user [30], pass [30], comando[30], *name;
-  int pid;
-
-   name = getlogin();
-
-   printf("Seja bem vindo %s!\n", name);
-
-   for(;;)
-   {
-      printf("fatec> ");
-      gets(comando);
-
-      makeTokens(comando);
-
-      argv[0] = strtok(comando, "");
-
-      if(strcmp(argv[0], "sair") == 0) {
-        printf("Até mais %s =)\n", name);
-        exit(0);
-      }
-
-      else if(strcmp(argv[0], "limpar") == 0 || strcmp(argv[0], "clear") == 0) printf("\e[H\e[2J");
-
-      else if(strcmp(argv[0], "ajuda") == 0) {
-        pid=fork();
-        if(pid==0) execvp("./app/ajuda", array);
-        else wait(NULL);
-        continue;
-      }
-
-      else if(strcmp(argv[0], "apagar") == 0) {
-        pid=fork();
-        if(pid==0) execlp("./app/apagar", "./app/apagar", array, NULL);
-        else wait(NULL);
-        continue;
-      }
-
-      else if(strcmp(argv[0], "arquivo") == 0) {
-        pid=fork();
-        if(pid==0) execvp("./app/arquivo", array);
-        else wait(NULL);
-        continue;
-      }
-
-      else if (strcmp(argv[0], "copiar") == 0) {
-        pid = fork();
-        if(pid==0) execvp("./app/copiar", array);
-        else wait(NULL);
-        continue;
-      }
-
-      else if(strcmp(argv[0], "criar") == 0) {
-        pid=fork();
-        if(pid==0) execvp("./app/criar", array);
-        else wait(NULL);
-        continue;
-      }
-
-      else if(strcmp(argv[0], "data") == 0) {
-        pid=fork();
-        if(pid==0) execvp("./app/data", array);
-        else wait(NULL);
-        continue;
-      }
-
-      else if(strcmp(argv[0], "dev") == 0) {
-        pid=fork();
-        if(pid==0) execlp("./app/dev", "./app/dev", array, NULL);
-        else wait(NULL);
-        continue;
-      }
-
-      else if (strcmp(argv[0], "listar") == 0) {
-        pid = fork();
-        if(pid==0) execvp("./app/listar", array);
-        else wait(NULL);
-        continue;
-      }
-
-      else if (strcmp(argv[0], "local") == 0) {
-        pid = fork();
-        if(pid == 0) execlp("./app/local", "./app/local", array, NULL);
-        else wait(NULL);
-        continue;
-      }
-
-      else if (strcmp(argv[0], "mudar") == 0) {
-        pid = fork();
-        if(pid==0) execvp("./app/mudar", array);
-        else wait(NULL);
-        continue;
-      }
-
-      else if (strcmp(argv[0], "usuario") == 0) {
-        pid = fork();
-        if(pid==0) execvp("./app/quem", array);
-        else wait(NULL);
-        continue;
-      }
-
-      else if(strcmp(argv[0], "remover") == 0) {
-        pid=fork();
-        if(pid==0) execlp("./app/remover", "./app/remover", array, NULL);
-        else wait(NULL);
-        continue;
-      }
-
-      else if(comando[0] == '.' && comando[1] == '/') {
-        system(comando);
-        continue;
-      }
-
-      else printf("comando: %s nao encontrado\n", comando);
-   }
 }
