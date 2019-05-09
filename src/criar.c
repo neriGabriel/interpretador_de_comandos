a3e8ed833d166bbf64d6730da7074762f10a8eb0
Gabriel de Oliveira Neri <neri.gabriel78@gmail.com>
2019-05-09 11:10:48 -0300

    Update criar.c

--- src/criar.c
+++ src/criar.c
@@ -5,14 +5,14 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-int main(void){
-  char diretorio[4020];
+int main(int argc, char * argv[]){
   int status;
-  printf("\e[H\e[2J");
-  printf("Entre com o diretorio que deseja criar: ");
-  gets(diretorio);
 
-  status = mkdir(diretorio, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+  if(argc < 2){
+    printf("criar [nome]\n");
+    return 0;
+  }
+  status = mkdir(argv[1], S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 
   if(status == 0) printf("Diretorio criado com sucesso! \n");
   else printf("Erro ao criar diretorio \n");
