7d6f2407f2aead856bea9f2b8739cb07eee4827b
MisterKaiou <kaio_soares_rauta@yahoo.com.br>
2019-05-09 16:43:14 -0300

    Testando chave SSH recem criada

--- src/shell.c
+++ src/shell.c
@@ -77,27 +77,16 @@ int main (int argc, char * argv[]){
       continue;
     }
   }
-void limpaTela(){printf("\e[H\e[2J");}
+void limpaTela(){
+  printf("\e[H\e[2J");
+}
 
 int main (int argc, char * argv[]){
   char user [30], pass [30], comando[30], *name;
   int pid;
 
-  limpaTela();
-
-  printf("Entre com o usuario: ");
-  gets(user);
-  printf("Entre com a senha: ");
-  gets(pass);
-
-   if(strcmp(user, "root") != 0 || strcmp(pass, "fatecso") != 0 )
-   {
-     printf("Usuario ou senha invalidos \n");
-     return 0;
-   }
-   limpaTela();
-
    name = getlogin();
+
    printf("Seja bem vindo %s!\n", name);
 
    for(;;)
