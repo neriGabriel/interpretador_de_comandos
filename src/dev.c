ff5b8daaf062b975c317ffc11677ab6068ed4bf7
Gabriel de Oliveira Neri <neri.gabriel78@gmail.com>
2019-05-20 21:08:11 -0300

    Update dev.c

--- src/dev.c
+++ src/dev.c
@@ -6,8 +6,8 @@ int main (void){
 	printf("\t ================== Creditos ==================\n");
 	printf("\t = - Desenvolvedores -           - RA -       =\n");
 	printf("\t = Gabriel de Oliveira    | RA: 0040961823021 =\n");
-  	printf("\t = Julio Guillen          | RA:               =\n");
-  	printf("\t = Matheus Marcucci       | RA:               =\n");
+  	printf("\t = Julio Guillen          | RA: 0040961823038 =\n");
+  	printf("\t = Matheus Marcucci       | RA: 0040961823019 =\n");
 	printf("\t = Kaio Rauta             | RA: 0040961823037 =\n");
   	printf("\t ==============================================\n");
 	return 0;
