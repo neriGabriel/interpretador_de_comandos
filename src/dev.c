43c7639c0bf748c40ebe4342aaf1a93e4b88ab7f
Gabriel de Oliveira Neri <neri.gabriel78@gmail.com>
2019-05-20 21:11:26 -0300

    Update dev.c

--- src/dev.c
+++ src/dev.c
@@ -3,12 +3,12 @@
 
 int main (void){
 
-	printf("\t ================== Creditos ==================\n");
-	printf("\t = - Desenvolvedores -           - RA -       =\n");
-	printf("\t = Gabriel de Oliveira    | RA: 0040961823021 =\n");
-  	printf("\t = Julio Guillen          | RA: 0040961823038 =\n");
-  	printf("\t = Matheus Marcucci       | RA: 0040961823019 =\n");
-	printf("\t = Kaio Rauta             | RA: 0040961823037 =\n");
-  	printf("\t ==============================================\n");
+	printf("\t ========================= Creditos ========================\n");
+	printf("\t = - Desenvolvedores -           	- RA -               =\n");
+	printf("\t = Gabriel 'Ghost' de Oliveira        | RA: 0040961823021  =\n");
+  	printf("\t = Julio 'Werewolf' Guillen           | RA: 0040961823038  =\n");
+  	printf("\t = Matheus 'Lightning' Marcucci       | RA: 0040961823019  =\n");
+	printf("\t = Kaio 'Poison' Rauta                | RA: 0040961823037  =\n");
+  	printf("\t ===========================================================\n");
 	return 0;
 }
